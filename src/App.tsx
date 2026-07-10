2fed2a25ca27981bb827a1c7135578d280223011
Shahriar Hasan Sayeed <sayedks007@yahoo.com>
2026-07-10 15:59:51 +0600

    feat:  Find Nearby Pool Prospects feature protected under supabase auth

--- src/App.tsx
+++ src/App.tsx
@@ -55,7 +55,7 @@ export default function App() {
           />        
           <Route
             path="/find-nearby-pool-prospects"
-            element={<OpportunityMapExperience />}
+            element={session ? <OpportunityMapExperience /> : <Navigate to="/signin" replace />}            
           />
           {/* Catch-all */}
           <Route
